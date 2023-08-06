a51c222826b54d7aec3e09c4dd27c205cb468df8
Sioood <theodupontpro@gmail.com>
2023-08-06 16:41:27 +0200

    🎉 first commit

---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+console.log("Hello World");
+</script>
+<template>
+  <main class="flex flex-col items-center">
+    <h1>Hello World</h1>
+
+    <div class="w-fit bg-background aspect-square">BACKGROUND</div>
+    <div class="w-fit bg-surface aspect-square">SURFACE</div>
+  </main>
+</template>
